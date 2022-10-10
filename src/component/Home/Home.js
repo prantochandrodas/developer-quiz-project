25a5c76a4b963b25d483abafbb7d35c0201dab00
Pranto <prantochandrodas@gmail.com>
2022-10-11 00:09:36 +0600

    modify header

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import HomeHeader from '../HomeHeader/HomeHeader';
-
+import './Home.css'
 const Home = () => {
     const topic=useLoaderData();
  
     console.log(topic)
     return (
-        <div>
+        <div className='home'>
             <HomeHeader></HomeHeader>
           {
             topic.data.map(cart=><Cart cart={cart}></Cart>)
--- src/component/HomeHeader/HomeHeader.js
+++ src/component/HomeHeader/HomeHeader.js
@@ -4,7 +4,12 @@ import './HomeHeader.css';
 const HomeHeader = () => {
     return (
         <div className='homeheader'>
-            <img src={headerImg} alt="" />
+            <div className='homeheader-details'>
+                <p>It is a Quiz site . That will help you to know your knowledge about developer.Try to give all the answer correct. </p>
+            </div>
+            <div className='homeheader-img'>
+                  <img src={headerImg} alt="" />
+            </div>
         </div>
     );
 };
