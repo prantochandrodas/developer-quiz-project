ae6263dce2552ea258d42a0680a6472cf17d8678
Pranto <prantochandrodas@gmail.com>
2022-10-10 21:25:20 +0600

    adding navbar

---
+++ src/component/Header/Header.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import './Header.css';
+function Header() {
+    return (
+        <Navbar className='bgNav' expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Developer-Quiz</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+
+                        <div className='styleLink'>
+                            
+                            <Link className='' to={'/'}>Home</Link>
+                            <Link to={'/blog'}>Blog</Link>
+                        </div>
+                      
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
