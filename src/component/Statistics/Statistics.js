ace8af75abfdf8e889e4edbe25fc7bc917d07bd9
Pranto <prantochandrodas@gmail.com>
2022-10-11 16:41:34 +0600

    fav icon add

--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
-import Chart from '../Chart/Chart';
+
 const Statistics = () => {
     const getData = useLoaderData();
     const data = getData.data;
     return (
-        <div >
+        <div>
+            <h2 className='text-center my-4'>Statistics of Total </h2>
              <ResponsiveContainer width="95%" height={400}>
              <LineChart  width={500} height={400} data={data}>
                 <Line type="monotone" dataKey="total" stroke="#8884d8" />
