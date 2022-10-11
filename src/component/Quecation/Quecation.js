03665683eb340676f84ea3a450ab388f8be083cf
Pranto <prantochandrodas@gmail.com>
2022-10-11 15:25:51 +0600

    add eye button toastify

--- src/component/Quecation/Quecation.js
+++ src/component/Quecation/Quecation.js
@@ -3,15 +3,27 @@ import { DEFAULT_BREAKPOINTS } from 'react-bootstrap/esm/ThemeProvider';
 import Option from '../Option/Option';
 import './Quecation.css';
 import { BeakerIcon ,EyeIcon} from '@heroicons/react/24/solid'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Quecation = ({ que }) => {
 //    console.log(que);
     const { question ,options,correctAnswer} = que;
     const splitque = question.split('<p>').join('');
     const newQue=splitque.split('</p>').join('');
   
-    const {ans}=que.correctAnswer;
    
+   const seeTheCorrectAns=ans=>{
+    toast.success(ans, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+   }
   
     return (
         <div className='container'>
@@ -19,7 +31,7 @@ const Quecation = ({ que }) => {
                 <div className='que'>
                     <h4> {newQue}</h4>
             
-                  <div className='que-icon'>
+                  <div className='que-icon' onClick={()=>seeTheCorrectAns(correctAnswer)}>
                      <EyeIcon className="h-25 w-75 text-blue-500"/>
                   </div>
                     
